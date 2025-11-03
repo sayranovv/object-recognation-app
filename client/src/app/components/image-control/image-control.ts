4816fb7c7608d18958e42cb544ceb5dccf9b5505
sayranovv <sayranoveldar@gmail.com>
2025-11-03 11:40:12 +0500

    Update image-control component

--- client/src/app/components/image-control/image-control.ts
+++ client/src/app/components/image-control/image-control.ts
@@ -1,18 +1,33 @@
 import {Component} from '@angular/core';
 import {Observable} from 'rxjs';
+import {Prediction} from '../../types/types';
+import {Upload} from '../../services/upload';
+import {AsyncPipe, PercentPipe, UpperCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-image-control',
-  imports: [],
+  imports: [
+    AsyncPipe,
+    UpperCasePipe,
+    PercentPipe
+  ],
   templateUrl: './image-control.html',
   styleUrl: './image-control.css',
 })
 export class ImageControl {
   imageUrl: string | null = null;
   selectedFile: File | null = null;
-  // prediction$: Observable<any[] | null>;
-  // loading$: Observable<boolean>;
-  // error$: Observable<string | null>;
+  prediction$: Observable<Prediction[] | null>;
+  loading$: Observable<boolean>;
+  error$: Observable<string | null>;
+
+  constructor(private uploadImageService: Upload) {
+    this.prediction$ = this.uploadImageService.prediction$
+    this.loading$ = this.uploadImageService.loadingSubject$
+    this.error$ = this.uploadImageService.error$
+
+
+  }
 
 
 

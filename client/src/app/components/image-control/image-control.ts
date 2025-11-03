16e9b39752aea44e5b8163d2dd961a35f14e3ade
sayranovv <sayranoveldar@gmail.com>
2025-11-03 11:59:43 +0500

    Create uploading image func

--- client/src/app/components/image-control/image-control.ts
+++ client/src/app/components/image-control/image-control.ts
@@ -1,8 +1,9 @@
 import {Component} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {Prediction} from '../../types/types';
 import {Upload} from '../../services/upload';
 import {AsyncPipe, PercentPipe, UpperCasePipe} from '@angular/common';
+import {api_key} from '../../keys/api_keys';
 
 @Component({
   selector: 'app-image-control',
@@ -33,6 +34,21 @@ export class ImageControl {
     if (file) {
       this.selectedFile = file
       this.imageUrl = URL.createObjectURL(file)
+    }
+  }
+
+  uploadImage(): void {
+    if (this.selectedFile) {
+      of(
+        this.uploadImageService
+          .uploadImage(this.selectedFile, `${api_key}/predict`)
+      ).subscribe({
+        next: (v) => (this.prediction$ = v),
+        error: (err) => (this.error$ = err),
+        complete: () => console.info('Complete')
+      })
+    } else {
+      console.error('No file selected')
     }
   }
 
